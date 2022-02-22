d6653d745b197b868241da592515e8eb12191b0b
nathanjones4323 <62448274+nathanjones4323@users.noreply.github.com>
2022-02-21 22:37:04 -0800

    Delete reduce_categories.py

--- functions/reduce_categories.py
+++
@@ -1,7 +0,0 @@
-# Version: Python 3.8.8 64-bit ('base': conda)
-
-def reduce_categories(character_column, n = 10):
-    ''' Reduces the number of unique categories/values present in a character/string variable to n, with the nth value as "other" '''
-    top_n_values = character_column.value_counts()[0:n].index.values
-    new_values = [x if x in top_n_values else "other" for x in character_column]
-    return new_values
